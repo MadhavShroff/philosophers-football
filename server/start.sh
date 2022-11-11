9f8db1d5a065f319d3d2e57e31aa84178df2e39c
MadhavShroff <madhavshroff99@gmail.com>
2022-11-11 02:29:08 -0500

    Updated startup script

--- server/start.sh
+++ server/start.sh
@@ -1,7 +1,7 @@
-cd /home/ubuntu/philosophers-football
+homeDir="/home/ubuntu/philosophers-football";
 
-git pull; npm install; npm run build;
+# git pull; npm install; npm run build;
 
-pm2 stop server/index.js; # stop server if running
+/home/ubuntu/.nvm/versions/node/v19.0.1/bin/pm2 stop $homeDir/server/index.js; # stop server if running
 
-pm2 start server/index.js --name "pf-server"; # start server
+/home/ubuntu/.nvm/versions/node/v19.0.1/bin/pm2 start $homeDir/server/index.js --name "pf-server"; # start server
