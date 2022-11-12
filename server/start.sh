dc0b715af64b3dd4476001ae6ff78fcbb5acb701
MadhavShroff <madhavshroff99@gmail.com>
2022-11-12 12:10:56 -0500

    Minor changes

--- server/start.sh
+++ server/start.sh
@@ -1,3 +1,3 @@
 projectDir="/home/ubuntu/philosophers-football";
 
-/home/ubuntu/.nvm/versions/node/v19.0.1/bin/pm2 start -f $projectDir/server/index.js --name "pf-server"; # start server
+/home/ubuntu/.nvm/versions/node/v19.0.1/bin/pm2 start $projectDir/server/index.js --name "pf-server"; # start server
