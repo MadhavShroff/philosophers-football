9a1a1b3c17305492f88cee5c021bdec846863026
MadhavShroff <madhavshroff99@gmail.com>
2022-11-12 05:16:39 -0500

    Change path to absolute path

--- server/start.sh
+++ server/start.sh
@@ -1,7 +1,7 @@
-# hocmeDir="/home/ubuntu/philosophers-football";
+projectDir="/home/ubuntu/philosophers-football";
 
 # git pull; npm install; npm run build;
 
-/home/ubuntu/.nvm/versions/node/v19.0.1/bin/pm2 stop $homeDir/server/index.js; # stop server if running
+/home/ubuntu/.nvm/versions/node/v19.0.1/bin/pm2 stop $projectDir/server/index.js; # stop server if running
 
-/home/ubuntu/.nvm/versions/node/v19.0.1/bin/pm2 start $homeDir/server/index.js --name "pf-server"; # start server
+/home/ubuntu/.nvm/versions/node/v19.0.1/bin/pm2 start $projectDir/server/index.js --name "pf-server"; # start server
