92951ee34f9686e36da8efccf64402e5e6ba2f13
MadhavShroff <madhavshroff99@gmail.com>
2022-11-14 23:05:08 -0500

    Minor Fix

--- server/index.js
+++ server/index.js
@@ -50,25 +50,22 @@ db.mongoose.connect(DB_HOST, {
 });
 
 function initial() {
-    Role.find({}, (err, roles) => {
-        if (err) {
-            console.log("error", err);
-        }
-        if (roles.length === 0) {
+    Role.estimatedDocumentCount((err, count) => {
+        if (!err && count === 0) {
             new Role({
                 name: "user"
             }).save(err => {
                 if (err) console.log("error", err);
                 console.log("added 'user' to roles collection");
             });
-            
+    
             new Role({
                 name: "moderator"
             }).save(err => {
                 if (err) console.log("error", err);
                 console.log("added 'moderator' to roles collection");
             });
-
+    
             new Role({
                 name: "admin"
             }).save(err => {
@@ -76,7 +73,7 @@ function initial() {
                 console.log("added 'admin' to roles collection");
             });
         } else {
-            console.log("Existing Roles: ", roles.map(role => role.name));
+            console.log(count + " Roles already exist");
         }
     });
 }
